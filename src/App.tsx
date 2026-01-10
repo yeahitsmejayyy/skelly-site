8097f76471f80f3d6579de7dc79fd011450e7481
yeahitsmejayyy <hello@itsjayyy.com>
2026-01-09 17:11:18 -0800

    chore(site): minor changes to display

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <div className="flex flex-col gap-4">
-        <p className="text-2xl">@skelly/site</p>
+        <h1 className="text-2xl flex flex-col items-center gap-1">
+          <span className="flex items-center justify-center border bg-background rounded-full w-[46px] h-[46px]">💀</span>
+          <span className="font-bold">skelly-site</span>
+        </h1>
         <div className="mx-auto">
           <ModeToggle />
         </div>
