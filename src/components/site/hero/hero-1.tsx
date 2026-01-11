dae94dab75b080f1f3cad90d9bdb964c0074a2b5
yeahitsmejayyy <hello@itsjayyy.com>
2026-01-11 12:07:37 -0800

    minor change to hero section header

--- src/components/site/hero/hero-1.tsx
+++ src/components/site/hero/hero-1.tsx
@@ -11,7 +11,7 @@ export default function Hero1() {
         <div className="w-full border flex items-center justify-center lg:py-50 py-30 lg:px-0 px-6">
             <div className="w-full flex flex-col gap-y-5 text-center mx-auto">
                 <p className="text-muted-foreground lg:text-[14px]">Hero Section</p>
-                <h1 className="lg:max-w-180 lg:text-6xl text-3xl font-bold mx-auto">Headline the solves user's main problem</h1>
+                <h1 className="lg:max-w-180 lg:text-6xl text-4xl font-bold mx-auto">Headline the solves user's main problem</h1>
                 <p className="lg:max-w-150 text-muted-foreground lg:text-[18px] mx-auto">
                     Follow with one or two sentences that expand on your value proposition. Focus on key benefits and address why users should take action now. Keep it scannable, short and benefit-driven.
                 </p>
