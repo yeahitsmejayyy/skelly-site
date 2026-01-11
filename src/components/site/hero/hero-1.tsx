a57f03e6ec921afd4d3fc692d5643f4b5e585072
yeahitsmejayyy <hello@itsjayyy.com>
2026-01-11 09:53:57 -0800

    minor changes to hero component

--- src/components/site/hero/hero-1.tsx
+++ src/components/site/hero/hero-1.tsx
@@ -9,13 +9,13 @@ import { ArrowRight } from "lucide-react";
 export default function Hero1() {
     return (
         <div className="w-full border flex items-center justify-center lg:py-50 py-30 lg:px-0 px-6">
-            <div className="w-full lg:max-w-150 flex flex-col gap-y-5 text-center mx-auto">
+            <div className="w-full flex flex-col gap-y-5 text-center mx-auto">
                 <p className="text-muted-foreground lg:text-[14px]">Hero Section</p>
-                <h1 className="lg:text-5xl text-3xl font-bold">Headline the solves user's main problem</h1>
-                <p className="text-muted-foreground lg:text-[18px]">
+                <h1 className="lg:max-w-180 lg:text-6xl text-3xl font-bold mx-auto">Headline the solves user's main problem</h1>
+                <p className="lg:max-w-150 text-muted-foreground lg:text-[18px] mx-auto">
                     Follow with one or two sentences that expand on your value proposition. Focus on key benefits and address why users should take action now. Keep it scannable, short and benefit-driven.
                 </p>
-                <Button className="w-fit mx-auto">Get access <ArrowRight /></Button>
+                <Button className="w-fit mx-auto" size={'lg'}>Get access <ArrowRight /></Button>
             </div>
         </div>
     );
