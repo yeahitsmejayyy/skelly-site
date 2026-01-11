35185cebd84cf6696e4ed8d9716a459f14001404
yeahitsmejayyy <hello@itsjayyy.com>
2026-01-10 18:14:44 -0800

    added who-its-for-1 section component

--- src/components/site/who-its-for/who-its-for-1.tsx
+++ src/components/site/who-its-for/who-its-for-1.tsx
@@ -3,28 +3,54 @@
 // }
 
 import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 import { ArrowRight } from "lucide-react";
 
 // props: WhoItsFor1Props 
 export default function WhoItsFor1() {
     return (
-        <div className="w-full flex items-center justify-center border">
-            <div className="grid lg:grid-cols-2 grid-cols-1 lg:gap-30 gap-y-[48px] w-325 lg:py-50 py-30 lg:px-0 px-12">
-                <div className="rounded-lg order-2">
-                    <img alt="Placeholder Image" src={'/square-placeholder.png'} className="rounded-lg" />
-                </div>
+        <div className="w-full border flex flex-col gap-y-12 items-center justify-center lg:py-50 py-30 lg:px-0 px-12">
+            <div className="w-full lg:max-w-170 flex flex-col gap-y-5 text-center mx-auto">
+                <p className="text-muted-foreground lg:text-[14px]">Hero Section</p>
+                <h1 className="lg:text-5xl text-3xl font-bold">Headline that shows testimonails for user success</h1>
+                <p className="text-muted-foreground lg:text-[18px]">
+                    Write a brief introduction that sets up your testimonials' credibility. Mention key outcomes, industry relevance, or the scale of your success.
+                </p>
+            </div>
+            <div className="lg:w-325 grid lg:grid-cols-3 grid-cols-1 gap-4">
+                <Card className="p-6">
+                    <p>"Shadcn UI Kit for Figma has completely transformed our design process. It’s incredibly intuitive and saves us so much time. The components are beautifully crafted and customizable.”</p>
 
-                <div className="order-2 flex flex-col justify-center lg:gap-y-[24px] gap-y-[24px]">
-                    <p className="text-muted-foreground lg:text-[14px]">Hero Section</p>
-                    <h1 className="lg:text-[38px] text-[30px] font-bold lg:leading-11 leading-8">Headline that shows who it's for and the user success</h1>
-                    <p className="text-muted-foreground lg:text-[18px]">
-                        Explain in one or two concise sentences how your solution transforms users' challenges into positive outcomes. Focus on the end benefits that matter most to your target audience. Keep it clear and compelling.
-                    </p>
                     <div className="flex items-center gap-x-2">
-                        <Button className="w-fit">Get access</Button>
-                        <Button variant={'ghost'} className="w-fit">Learn more <ArrowRight size={'20'} /></Button>
+                        <img alt="Avatar Image" src={'/avatar.png'} className="w-[42px] h-[42px] rounded-full" />
+                        <div className="flex flex-col gap-y-0">
+                            <p className="font-bold">Jack Bauer</p>
+                            <p>Founder at Acme Inc</p>
+                        </div>
                     </div>
-                </div>
+                </Card>
+                <Card className="p-6">
+                    <p>"Shadcn UI Kit for Figma has completely transformed our design process. It’s incredibly intuitive and saves us so much time. The components are beautifully crafted and customizable.”</p>
+
+                    <div className="flex items-center gap-x-2">
+                        <img alt="Avatar Image" src={'/avatar.png'} className="w-[42px] h-[42px] rounded-full" />
+                        <div className="flex flex-col gap-y-0">
+                            <p className="font-bold">Jack Bauer</p>
+                            <p>Founder at Acme Inc</p>
+                        </div>
+                    </div>
+                </Card>
+                <Card className="p-6">
+                    <p>"Shadcn UI Kit for Figma has completely transformed our design process. It’s incredibly intuitive and saves us so much time. The components are beautifully crafted and customizable.”</p>
+
+                    <div className="flex items-center gap-x-2">
+                        <img alt="Avatar Image" src={'/avatar.png'} className="w-[42px] h-[42px] rounded-full" />
+                        <div className="flex flex-col gap-y-0">
+                            <p className="font-bold">Jack Bauer</p>
+                            <p>Founder at Acme Inc</p>
+                        </div>
+                    </div>
+                </Card>
             </div>
         </div>
     );
