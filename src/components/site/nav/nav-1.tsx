d5410f24a30f33347b7a16615e3a99142e33b83b
yeahitsmejayyy <hello@itsjayyy.com>
2026-01-11 12:21:37 -0800

    modified nav1 component to activate sheet for mobile menu

--- src/components/site/nav/nav-1.tsx
+++ src/components/site/nav/nav-1.tsx
@@ -4,12 +4,13 @@
 
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
 
 // props: Nav1Props 
 export default function Nav1() {
     return (
-        <div className="fixed right-0 left-0 lg:top-4 top-0 w-full h-16.25 max-w-215 mx-auto px-5 flex items-center justify-between bg-background/60 backdrop-blur-md backdrop-saturate-150
+        <div className="fixed right-0 left-0 lg:top-4 top-0 w-full h-16.25 max-w-215 mx-auto px-5 flex items-center justify-between lg:bg-background/60 bg-background/85 backdrop-blur-md backdrop-saturate-150
 border border-border/50
 lg:rounded-lg lg:shadow-lg">
             <div className="flex items-center gap-1">
@@ -30,7 +31,26 @@ lg:rounded-lg lg:shadow-lg">
                 <div className="lg:block hidden">
                     <ModeToggle />
                 </div>
-                <Button variant={'ghost'} size={'icon'} className="lg:hidden flex"><Menu size={'20px'} /></Button>
+                <div className="lg:hidden block">
+                    <ModeToggle />
+                </div>
+                <Sheet>
+                    <SheetTrigger>
+                        <Button variant={'ghost'} size={'icon'} className="lg:hidden flex"><Menu size={'20px'} /></Button>
+                    </SheetTrigger>
+                    <SheetContent className="p-6">
+                        <a href="#">How It Works</a>
+                        <a href="#">Features</a>
+                        <a href="#">Pricing</a>
+                        <Button className="w-full">Get started</Button>
+                        {/* <DropdownMenuItem className="outline-none">How It Works</DropdownMenuItem>
+                        <DropdownMenuItem className="outline-none">Features</DropdownMenuItem>
+                        <DropdownMenuItem className="outline-none">Pricing</DropdownMenuItem>
+                        <DropdownMenuItem className="outline-none">
+                            <Button className="flex" size={'lg'}>Get Started</Button>
+                        </DropdownMenuItem> */}
+                    </SheetContent>
+                </Sheet>
             </div>
         </div>
     );
